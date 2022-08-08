1b0cc3640a6ebc2156dda76ac04c48299303f2ce
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-08-08 14:50:29 -0700

    Update

---
+++ myQuiz.py
@@ -0,0 +1,18 @@
+
+
+
+Statement A
+import Budget
+
+Statement B
+import Budget;print(calcBills0)
+
+Statement C
+import Budget;print(Budget.calBills0)
+
+Statement D
+print(Budget.calBills0)
+
+
+
+import Budget;print(Budget.calBills0)
