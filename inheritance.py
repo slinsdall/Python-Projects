af1b5eab4dbaab222d2341b0b7db5a65c299916a
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-08-12 14:46:25 -0700

    Create inheritance.py
    Inheritance Assignment.

---
+++ inheritance.py
@@ -0,0 +1,14 @@
+class User:
+    name = 'Enter Name '
+    email = 'Enter Email '
+    password = 'Enter Password'
+
+class Teacher(User):
+    ID = 115
+    Class = 'Computer Science'
+
+class Student(User):
+    Major = 'Computer Science '
+    Grade = 'B+'
+
+    
