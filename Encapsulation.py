a026b151432d694ae480ecd59ea5fc2b3172d52c
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-08-19 15:10:20 -0700

    Update

--- Encapsulation.py
+++ Encapsulation.py
@@ -4,19 +4,12 @@
 class Encapsulation:
     def __init__(self):
         self.protectedVar = 36 #This is the protected Variable
-
-obj = Encapsulation() #This defines the obj(object)
-obj._protectedVar = 36
-print(obj._protectedVar)
-
-class Encapsulation:
-    def __init__(self):
         self.__privateVar = 14 #Private is done with double __ and is harder to access.
 
     def getPrivate(self):
         print(self.__privateVar)#This will print the variable we have given it on the screen in shell
 
-
-obj = Encapsulation()
+obj = Encapsulation() #This defines the obj(object)
+obj._protectedVar = 36
 obj.getPrivate()
-
+print(obj._protectedVar)
