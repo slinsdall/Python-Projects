ab431eb21ab36022a084b66bfd1fb94fc0453f62
slinsdall <105024420+slinsdall@users.noreply.github.com>
2022-09-05 09:55:08 -0700

    Update

--- sqliteAssignment.py
+++ sqliteAssignment.py
@@ -1,15 +1,9 @@
 import sqlite3
-connection = sqlite3.connect(r"C:\Users\slins\OneDrive\Documents\GitHub\Tech-Academy-Projects\Python-Projects\test_database.db")
-c = connection.cursor()
-c.execute("CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT)")
-c.execute("INSERT INTO People VALUES('Ron', 'Obvious', 42)")
-connection.commit()
-
-connection = sqlite3.connect(':memory:')
-c.execute("DROP TABLE IF EXISRS People")
-connection.close()
-
-with sqlite3.connect("test_database.db") as connection:
-# perform any SQL operations using connection here
-
-
+with sqlite3.connect('test_database.db') as connection:
+    c = connection.cursor()
+    c.executescript("""DROP TABLE IF EXISTS People;
+                    CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT);
+                    INSERT INTO People VALUES('Ron', 'Obvious', '42');
+                    """)
+    peopleValues = (('Luigi', 'Vercotti', 43), ('Arthur', 'Belling', 28))
+    c.executemany("INSERT INTO People VALUES(?, ?, ?,)", peopleValues)
